232c2cf602ff10b0d40f208ee5c58847498f97ab
crueter <crueter@eden-emu.dev>
2026-01-02 22:08:41 -0500

    fix sccache fr maybe, openbsd
    Signed-off-by: crueter <crueter@eden-emu.dev>

--- tools/common.sh
+++ tools/common.sh
@@ -70,7 +70,7 @@ extract() {
 	esac
 
 	# qt6windows7 patch
-	if [ "$VERSION" = "$QT6WINDOWS7_VERSION" ]; then
+	if [ "$VERSION" = "$QT6WINDOWS7_VERSION" ] && [ "$PLATFORM" != openbsd ]; then
 		echo "-- Patching for Windows 7..."
 
 		curl -L "$QT6WINDOWS7_URL" -o w7.tar.gz
