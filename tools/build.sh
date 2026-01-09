6f4906212bcdc8dc171e735308d28ca396f19920
crueter <crueter@eden-emu.dev>
2026-01-09 15:00:05 -0500

    Also enable xml
    Signed-off-by: crueter <crueter@eden-emu.dev>

--- tools/build.sh
+++ tools/build.sh
@@ -114,7 +114,7 @@ configure() {
 		echo "-- * FFmpeg dir: $FFMPEG_DIR"
 		echo "-- * OpenSSL dir: $OPENSSL_DIR"
 	fi
-	
+
 	# if windows; then
 	# 	FFMPEG_DIR="$(cygpath -w "$FFMPEG_DIR")"
 	# 	OPENSSL_DIR="$(cygpath -w "$OPENSSL_DIR")"
@@ -173,7 +173,7 @@ configure() {
 		-submodules "$SUBMODULES" \
 		-skip qtlanguageserver,qtquicktimeline,qtactiveqt,qtquick3d,qtquick3dphysics,qtdoc,qt5compat \
 		-no-feature-icu -release -no-zstd -no-feature-qml-network -no-feature-libresolv -no-feature-dladdr \
-		-no-feature-sql -no-feature-xml -no-feature-printdialog -no-feature-printer -no-feature-printsupport \
+		-no-feature-sql -no-feature-printdialog -no-feature-printer -no-feature-printsupport \
 		-no-feature-linguist -no-feature-designer -no-feature-assistant -no-feature-pixeltool -feature-filesystemwatcher -- "$@" \
 		-DCMAKE_CXX_FLAGS="$FLAGS" -DCMAKE_C_FLAGS="$FLAGS" -DCMAKE_OSX_DEPLOYMENT_TARGET="${MACOSX_DEPLOYMENT_TARGET}" \
 		-DCMAKE_EXE_LINKER_FLAGS="$LDFLAGS"
